5f159ce70a342fd29e3595a558cd1cc80b777182
aasif-codes <muhammed.aasif@outlook.com>
2026-02-28 07:15:49 +0530

    Add files via upload

---
+++ Java Script/learn.js
@@ -0,0 +1,64 @@
+// practice 1
+let i = 1;
+let result = i++ + (i++ + ++i);
+console.log(i, result);  
+ 
+// practice 2
+
+
+ function Apple()
+{
+    console.log("IPhone 15Pro Max")
+} 
+
+
+function Xioami()
+{
+    console.log ("Xioami 17Pro Max")
+}
+
+
+function Google ()
+{
+    console.log ("Pixel 9A")
+}
+
+Apple(),Google(),Xioami(); 
+
+
+// practice 3
+
+
+let a = 20, b = 25;
+
+function additon()
+{
+    console.log(a+b);
+}
+
+additon()
+ 
+// practice 4
+
+let fav_color = "Blue", fav_fruite = "Orange", fav_hobby = "Coding";
+
+function fav() 
+{
+    console.log("Favorite color:" + fav_color);
+    console.log("Favorite fruite:" + fav_fruite);
+    console.log("Favorite hobby:" + fav_hobby);
+
+}
+
+fav()  
+
+// practice 5
+
+ function Area(l,w)
+{
+    console.log("Aria ="+(l*w) );
+}
+Area(15,10);
+
+
+// Complete 5 practice today
