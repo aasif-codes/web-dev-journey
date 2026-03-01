2bfa1fc0ac1b390dcc04e3f82d1873e98548aa83
aasif-codes <muhammed.aasif@outlook.com>
2026-03-01 07:24:42 +0530

    Add files via upload

---
+++ Java Script/script.js
@@ -0,0 +1,104 @@
+ // # Practice 1 ( Return )
+
+function add(a,b)
+{
+    return a+b
+}
+
+ total = add(30,40)
+ console.log(total)
+
+ // 2nd
+
+ function carname()
+ {
+    return "Lambho-Aventador-SVJ"
+ }
+ car = carname()
+ console.log(car)
+
+//3rd 
+function test(x) {
+  if (x > 5) {
+    return x * 2;
+  }
+  return x + 5;
+}
+
+console.log(test(4));  //output = 9 ,  12
+console.log(test(6));
+
+
+//4 
+function add(a,b)
+{
+    return (a+b)
+    console.log(a+b)  // because return take over the fucntion bro 
+
+}
+ sum = add(10,25)
+ console.log(sum);
+ 
+
+
+ 
+
+// # Practice 2 (if,else)
+// 1
+
+if (false)
+{
+  console.log("Yes")
+}
+else {
+    console.log("No")
+} 
+
+
+// 2
+ var homework = true
+if(homework)
+{
+    console.log(" Good job!")
+}
+else {
+    console.log("finish before you take the console")
+}
+ 
+// 3 
+
+var cookies = false
+if(cookies)
+{
+    console.log('available')
+}
+else {
+    console.log("Not Available")
+} 
+
+
+// 4
+var a= 10+20
+if(a=30)
+{
+    console.log("Correct !")
+}
+else {
+    console.log("Wrong !")
+}
+ 
+
+// 5 
+
+let add = 1000n-1n
+if(add=999n)
+{
+    console.log("Correct")
+}
+
+else {
+    console.log ("wrong !")
+}
+
+
+
